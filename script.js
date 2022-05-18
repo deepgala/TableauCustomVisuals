807e8ffefab45db16413e48d7b819ec712f0a48c
deepgala <60201059+deepgala@users.noreply.github.com>
2022-05-18 13:56:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-// Add you awesome code here!
-console.log('Hello 🌎');
+/* global tableau Chart */
+tableau.extensions.initializeAsync({ configure: configure }).then(() => {
+  // ...
+});
+
+async function configure() {
+  try {
+    const url = `${window.location.origin}/config.html`;
+    await tableau.extensions.ui.displayDialogAsync(url, "", {
+      width: 500,
+      height: 600
+    });
+    // ... more to come here ...
+  } catch (error) {
+    switch (error.errorCode) {
+      case tableau.ErrorCodes.DialogClosedByUser:
+        console.log("Dialog was closed by user.");
+        break;
+      default:
+        console.error(error.message);
+    }
+  }
+}
