ecabeb186448a2ee0c04f0cba6ddcd725c14dc76
deepgala <60201059+deepgala@users.noreply.github.com>
2022-05-18 14:00:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,71 @@ async function configure() {
     }
   }
 }
+
+async function getData() {
+  const settings = tableau.extensions.settings.getAll();
+  const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
+  const worksheet = worksheets.find(ws => ws.name === settings.sourceWorksheet);
+  const dataTable = await worksheet.getSummaryDataAsync();
+  const dimensionFieldIndex = dataTable.columns.find(
+        column => column.fieldName === settings.dimensionField
+      ).index;
+    const measureFieldIndex = dataTable.columns.find(
+        column => column.fieldName === settings.measureField
+      ).index;
+
+  let data = [];
+
+  for (let row of dataTable.data) {
+    data.push({
+      label: row[dimensionFieldIndex].value,
+      value: row[measureFieldIndex].value
+    });
+  }
+
+  data.sort((a, b) => (a.label > b.label ? 1 : -1));
+
+  drawChart(data);
+}
+
+function drawChart(vizData) {
+  let values = vizData.map(row => row.value);
+  let labels = vizData.map(row => row.label);
+
+  let ctx = document.getElementById("canvas").getContext("2d");
+
+  let data = {
+    datasets: [
+      {
+        data: values,
+        backgroundColor: "#4E79A7"
+      }
+    ],
+    labels: labels
+  };
+
+  let options = {
+    onClick: filter,
+    maintainAspectRatio: false
+  };
+
+  let polarChart = new Chart(ctx, {
+    data: data,
+    type: "polarArea",
+    options: options
+  });
+}
+
+function filter(event, item) {
+  const settings = tableau.extensions.settings.getAll();
+  const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
+  const worksheet = worksheets.find(ws => ws.name === settings.filterWorksheet);
+
+  if (item[0]) {
+    let index = item[0].index;
+    let label = event.chart.data.labels[index];
+    worksheet.applyFilterAsync(settings.dimensionField, [label], "replace");
+  } else {
+    worksheet.clearFilterAsync(settings.dimensionField);
+  }
+}
